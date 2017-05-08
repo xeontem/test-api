3b4b3154b07428eb7082f651330bfc9f578af207
xeontem <xeontem@gmail.com>
2017-05-08 19:59:22 +0300

    test-api/feature: first push

--- js/bundle.js
+++ js/bundle.js
@@ -225,6 +225,17 @@ var Request = function () {
 	return Request;
 }();
 
+// function showInfo(message){
+//     document.querySelector('#auth-status').style.opacity = '1';
+//     document.querySelector('#auth-status').innerHTML = message;
+//     setTimeout(decreaseOpacity, 5000);
+// }
+
+// function decreaseOpacity(){
+//     document.querySelector('#auth-status').style.opacity = '0';
+// }
+
+
 exports.default = Request;
 
 /***/ }),
@@ -721,6 +732,94 @@ var GASection = function () {
     }
 
     _createClass(GASection, [{
+        key: 'requestOAuth',
+        value: function requestOAuth() {
+            var GoogleAuth = void 0;
+            var SCOPE = 'https://www.googleapis.com/auth/youtube.force-ssl';
+            function handleClientLoad() {
+                // Load the API's client and auth2 modules.
+                // Call the initClient function after the modules load.
+                gapi.load('client:auth2', initClient);
+            }
+
+            function initClient() {
+                // Retrieve the discovery document for version 3 of YouTube Data API.
+                // In practice, your app can retrieve one or more discovery documents.
+                var discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest';
+
+                // Initialize the gapi.client object, which app uses to make API requests.
+                // Get API key and client ID from API Console.
+                // 'scope' field specifies space-delimited list of access scopes.
+                gapi.client.init({
+                    'apiKey': 'AIzaSyA17KYHw-TfsiBy3TdT8hThejNcLjdNnOo',
+                    'discoveryDocs': [discoveryUrl],
+                    'clientId': '556623487116-evicb9k95c2fdspip898br18n6ugh8qn.apps.googleusercontent.com',
+                    'scope': SCOPE
+                }).then(function () {
+                    GoogleAuth = gapi.auth2.getAuthInstance();
+
+                    // Listen for sign-in state changes.
+                    GoogleAuth.isSignedIn.listen(updateSigninStatus);
+
+                    // Handle initial sign-in state. (Determine if user is already signed in.)
+                    var user = GoogleAuth.currentUser.get();
+                    setSigninStatus();
+
+                    // Call handleAuthClick function when user clicks on
+                    //      "Sign In/Authorize" button.
+
+                    document.querySelector('#sign-in-or-out-button').addEventListener('click', function (e) {
+                        handleAuthClick();
+                    });
+                    document.querySelector('#revoke-access-button').addEventListener('click', function (e) {
+                        revokeAccess();
+                    });
+                    // $('#sign-in-or-out-button').click(function() {
+                    // }); 
+                    // $('#revoke-access-button').click(function() {
+                    // }); 
+                });
+            }
+
+            function handleAuthClick() {
+                if (GoogleAuth.isSignedIn.get()) {
+                    // User is authorized and has clicked 'Sign out' button.
+                    GoogleAuth.signOut();
+                } else {
+                    // User is not signed in. Start Google auth flow.
+                    GoogleAuth.signIn();
+                }
+            }
+
+            function revokeAccess() {
+                GoogleAuth.disconnect();
+            }
+
+            function setSigninStatus(isSignedIn) {
+                var user = GoogleAuth.currentUser.get();
+                var isAuthorized = user.hasGrantedScopes(SCOPE);
+                if (isAuthorized) {
+                    GASection.setInfo(user); // fill info user panel...
+
+                    document.querySelector('.auth-section div').style.display = 'block';
+                    document.querySelector('#sign-in-or-out-button').innerHTML = 'Sign out';
+                    document.querySelector('#revoke-access-button').style.display = 'inline-block';
+                    showInfo('You are currently signed in and have granted access to this app.');
+                } else {
+                    document.querySelector('.auth-section div').style.display = 'none';
+                    document.querySelector('#sign-in-or-out-button').innerHTML = 'Sign In';
+                    document.querySelector('#revoke-access-button').style.display = 'none';
+                    showInfo('You have not authorized this app or you are signed out.');
+                }
+            }
+
+            function updateSigninStatus(isSignedIn) {
+                setSigninStatus();
+            }
+
+            handleClientLoad(); // add listener to sign in button
+        }
+    }], [{
         key: 'setInfo',
         value: function setInfo(responce) {
             //console.log(responce);
@@ -895,6 +994,16 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+window.showInfo = function (message) {
+    document.querySelector('#auth-status').style.opacity = '1';
+    document.querySelector('#auth-status').innerHTML = message;
+    setTimeout(decreaseOpacity, 5000);
+};
+
+function decreaseOpacity() {
+    document.querySelector('#auth-status').style.opacity = '0';
+}
+
 var Main = function () {
     function Main() {
         _classCallCheck(this, Main);
@@ -970,8 +1079,9 @@ var Main = function () {
     return Main;
 }();
 
-window.main = new Main();
-window.main.addSearchListeners();
+var main = new Main();
+main.addSearchListeners();
+main.GASection.requestOAuth();
 
 /***/ })
 /******/ ]);
